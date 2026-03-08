80c56fff99da8d7b9da96bea1a17d88d1bcfb01b
Naveen Kumar S <146418107+Naveen034@users.noreply.github.com>
2026-03-08 21:33:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
-const text = "Software Developer | Java | Python | Web Development"
+window.addEventListener("load",function(){
 
-let i = 0
+document.getElementById("loader").style.display="none"
 
-function typing(){
+})
 
-document.querySelector(".typing").textContent = text.slice(0,i)
+var typed = new Typed(".typing",{
 
-i++
+strings:[
+"Software Developer",
+"DevOps Enthusiast",
+"Java Developer",
+"Machine Learning Explorer"
+],
 
-if(i <= text.length){
+typeSpeed:60,
+backSpeed:40,
+loop:true
 
-setTimeout(typing,80)
+})
+
+const canvas = document.getElementById("cloud")
+const ctx = canvas.getContext("2d")
+
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+
+let particles=[]
+
+for(let i=0;i<80;i++){
+
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+r:Math.random()*2
+})
 
 }
 
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+particles.forEach(p=>{
+
+ctx.beginPath()
+ctx.arc(p.x,p.y,p.r,0,Math.PI*2)
+ctx.fillStyle="white"
+ctx.fill()
+
+})
+
+requestAnimationFrame(animate)
+
 }
 
-typing()
+animate()
